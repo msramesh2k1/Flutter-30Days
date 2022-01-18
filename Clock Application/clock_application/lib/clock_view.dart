bc60bb4920d26cb9a148387c526798a7fc1bf090
Ramesh M S <msramesh2k1@gmail.com>
2022-01-18 12:48:37 +0530

    feat:Clock Rotate

--- Clock Application/clock_application/lib/clock_view.dart
+++ Clock Application/clock_application/lib/clock_view.dart
@@ -15,8 +15,11 @@ class _ClockViewState extends State<ClockView> {
     return Container(
       height: 300,
       width: 300,
-      child: CustomPaint(
-        painter: ClockPainter(),
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
       ),
     );
   }
@@ -25,6 +28,7 @@ class _ClockViewState extends State<ClockView> {
 class ClockPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    var Datetime = DateTime.now();
     var centerX = size.width / 2;
     var centerY = size.height / 2;
     var center = Offset(centerX, centerY);
@@ -55,14 +59,16 @@ class ClockPainter extends CustomPainter {
     canvas.drawCircle(center, radius - 40, paintbrush);
 
     canvas.drawCircle(center, radius - 40, outlinebrush);
-    var secondX = centerX + 80 * cos(90 * pi / 180);
-    var secondY = centerX + 80 * sin(90 * pi / 180);
+    var secondX = centerX + 80 * cos(Datetime.second * 6 * pi / 180);
+    var secondY = centerX + 80 * sin(Datetime.second * 6 * pi / 180);
     canvas.drawLine(center, Offset(secondX, secondY), sechandbrush);
-    var minX = centerX + 80 * cos(45 * pi / 180);
-    var minY = centerX + 80 * sin(45 * pi / 180);
+    var minX = centerX + 80 * cos(Datetime.minute * 6 * pi / 180);
+    var minY = centerX + 80 * sin(Datetime.minute * 6 * pi / 180);
     canvas.drawLine(center, Offset(minX, minY), minhandbrush);
-    var hourX = centerX + 60 * cos(180 * pi / 180);
-    var hourY = centerX + 60 * sin(180 * pi / 180);
+    var hourX = centerX +
+        60 * cos((Datetime.hour * 30 + Datetime.minute * 0.5) * pi / 180);
+    var hourY = centerX +
+        60 * sin((Datetime.hour * 30 + Datetime.minute * 0.5) * pi / 180);
     canvas.drawLine(center, Offset(hourX, hourY), hourhandbrush);
     canvas.drawCircle(center, 8, outlinebrush);
   }
